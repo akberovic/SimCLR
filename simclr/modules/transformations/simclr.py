50ffe4ea4308939ec36ef7e9cd1984336fb96fe7
Anar Akbarov <100271133+akberovic@users.noreply.github.com>
2025-04-17 11:45:52 -0400

    feat(transform): add RandomSharpness augmentation and include in TransformsSimCLR
    +# at the top of the file, add these imports
    +import random
    +from PIL import ImageFilter
    
     class RandomSharpness(object):
         """
         Apply a sharpen filter to the image with a probability of p.
         """
         def __init__(self, p=0.5):
             self.p = p
    
         def __call__(self, img):
             if random.random() < self.p:
                 return img.filter(ImageFilter.SHARPEN)
             return img
    
    @@ -XX,7 +XX,12 @@ class TransformsSimCLR(object):
             transforms.RandomResizedCrop(size=size),
             transforms.RandomHorizontalFlip(),
    -        transforms.RandomGrayscale(p=0.2),
    +        transforms.RandomGrayscale(p=0.2),
    +        transforms.RandomApply([
    +            transforms.GaussianBlur(
    +                kernel_size=(int(0.1 * size) // 2 * 2 + 1)
    +            )
    +        ], p=0.5),                                # 50% chance Gaussian blur
    +        transforms.RandomApply([RandomSharpness(p=1.0)], p=0.5),  # 50% chance sharpen
    
             transforms.ToTensor(),
             transforms.Normalize(mean=mean, std=std),

--- simclr/modules/transformations/simclr.py
+++ simclr/modules/transformations/simclr.py
@@ -1,34 +1,50 @@
-import torchvision
+import torchvision.transforms as transforms
+from PIL import ImageFilter
+import random
 
+class RandomSharpness(object):
+    """
+    Apply a sharpen filter to the image with a probability of p.
+    """
+    def __init__(self, p=0.5):
+        self.p = p
+
+    def __call__(self, img):
+        if random.random() < self.p:
+            return img.filter(ImageFilter.SHARPEN)
+        return img
 
 class TransformsSimCLR:
     """
-    A stochastic data augmentation module that transforms any given data example randomly
-    resulting in two correlated views of the same example,
-    denoted x ̃i and x ̃j, which we consider as a positive pair.
+    A stochastic data augmentation module that transforms any given data example randomly,
+    resulting in two correlated views of the same example, denoted x̃i and x̃j,
+    which we consider as a positive pair.
     """
-
     def __init__(self, size):
         s = 1
-        color_jitter = torchvision.transforms.ColorJitter(
+        color_jitter = transforms.ColorJitter(
             0.8 * s, 0.8 * s, 0.8 * s, 0.2 * s
         )
-        self.train_transform = torchvision.transforms.Compose(
-            [
-                torchvision.transforms.RandomResizedCrop(size=size),
-                torchvision.transforms.RandomHorizontalFlip(),  # with 0.5 probability
-                torchvision.transforms.RandomApply([color_jitter], p=0.8),
-                torchvision.transforms.RandomGrayscale(p=0.2),
-                torchvision.transforms.ToTensor(),
-            ]
-        )
+        self.train_transform = transforms.Compose([
+            transforms.RandomResizedCrop(size=size),
+            transforms.RandomHorizontalFlip(),          # Horizontal flip with 0.5 probability
+            transforms.RandomVerticalFlip(),            # Added vertical flip
+            transforms.RandomRotation(degrees=15),      # Added random rotation within ±15°
+            transforms.RandomApply([color_jitter], p=0.8),# Color jitter
+            transforms.RandomGrayscale(p=0.2),           # Random grayscale conversion
+            transforms.RandomApply([
+                transforms.GaussianBlur(
+                    kernel_size=(int(0.1 * size) // 2 * 2 + 1)
+                )
+            ], p=0.5),                                # Added Gaussian blur with 50% probability
+            transforms.RandomApply([RandomSharpness(p=1.0)], p=0.5), # Added random sharpness with 50% probability
+            transforms.ToTensor(),
+        ])
 
-        self.test_transform = torchvision.transforms.Compose(
-            [
-                torchvision.transforms.Resize(size=size),
-                torchvision.transforms.ToTensor(),
-            ]
-        )
+        self.test_transform = transforms.Compose([
+            transforms.Resize(size=size),
+            transforms.ToTensor(),
+        ])
 
     def __call__(self, x):
         return self.train_transform(x), self.train_transform(x)
